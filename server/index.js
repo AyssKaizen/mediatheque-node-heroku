a64e76d548697e95ec0097af1d6f2c56f9531f42
Aïssa <82447520+AyssKaizen@users.noreply.github.com>
2021-11-26 13:48:09 +0100

    error path index.js server

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../mediatheque/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
