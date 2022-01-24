686ee689e7ded13aedcd61000e7c7501066febeb
Aïssa <82447520+AyssKaizen@users.noreply.github.com>
2022-01-24 16:49:20 +0100

    fixture knex changed

--- data/migrations/20211216191422_first-migration.js
+++ data/migrations/20211216191422_first-migration.js
@@ -1,12 +1,21 @@
 exports.up = (knex) => {
     return knex.schema
-        .createTable('profiles', function (table) {
-            table.increments('profile_id');
-            table.string('name', 128).notNullable();
+        .createTable('users', function (table) {
+            table.increments('us_id');
+            table.string('us_lastname', 128).notNullable();
+            table.string('us_firstname', 128).notNullable();
+            table.string('us_email', 128).notNullable();
+            table.date('us_birthday');
+            table.string('us_address', 128).notNullable();
+            table.string('us_city', 128).notNullable();
+            table.string('us_postcode', 128).notNullable();
+            table.boolean('us_active');
+            table.string('us_password', 128).notNullable();
+            table.boolean('us_admin');
         });
     };
 
 exports.down = (knex) => {
-    return knex.schema.dropTableIfExists('profiles');
+    return knex.schema.dropTableIfExists('users');
 };
 
--- data/seeds/001_first-seed.js
+++ data/seeds/001_first-seed.js
@@ -1,24 +1,69 @@
-const profiles = [
+const users = [
   {
-    name: 'John'
+    us_lastname: 'Jaeger',
+    us_firstname: 'Eren',
+    us_email: 'eren@mail.fr',
+    us_birthday: '1990-01-01',
+    us_address: '24 chemin du lion',
+    us_city: 'Lyon',
+    us_postcode: '69200',
+    us_active: true,
+    us_password: 'eren123',
+    us_admin: false
   },
   {
-    name: 'Abigail'
+    us_lastname: 'Ackerman',
+    us_firstname: 'Mikasa',
+    us_email: 'mikasa@mail.fr',
+    us_birthday: '1990-09-01',
+    us_address: '24 chemin du shingashina',
+    us_city: 'Shiganshina',
+    us_postcode: '31200',
+    us_active: true,
+    us_password: 'mikasa123',
+    us_admin: false
   },
   {
-    name: 'Patty'
+    us_lastname: 'Braun',
+    us_firstname: 'Reiner',
+    us_email: 'reiner@mail.fr',
+    us_birthday: '1990-01-01',
+    us_address: '24 chemin de mar',
+    us_city: 'Mar',
+    us_postcode: '31000',
+    us_active: true,
+    us_password: 'reiner123',
+    us_admin: false
   },
   {
-    name: 'Sally'
+    us_lastname: 'Ackerman',
+    us_firstname: 'Livai',
+    us_email: 'livai@mail.fr',
+    us_birthday: '1990-01-01',
+    us_address: '24 chemin des bas fonds',
+    us_city: 'Shiganshina',
+    us_postcode: '69000',
+    us_active: true,
+    us_password: 'livai123',
+    us_admin: false
   },
   {
-    name: 'Gary'
-  }
+    us_lastname: 'Arlet',
+    us_firstname: 'Armin',
+    us_email: 'armin@mail.fr',
+    us_birthday: '1990-01-01',
+    us_address: '34 chemin de trost',
+    us_city: 'Shiganshina',
+    us_postcode: '31000',
+    us_active: true,
+    us_password: 'armin123',
+    us_admin: false
+  },
 ];
 
 exports.seed = function (knex) {
-  return knex('profiles').del()
+  return knex('users').del()
   .then(() => {
-    return knex('profiles').insert(profiles)
+    return knex('users').insert(users)
   })
 };
